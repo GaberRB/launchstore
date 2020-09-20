58a769cc5c44b5d544ca0ef028b8d79fbd60ba7b
gabriel <gabriel@pop-os.localdomain>
2020-09-20 19:33:12 -0300

    final upload imagens

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,7 +1,8 @@
-const {formatPrice} = require ('../../lib/utils')
+const {formatPrice, date} = require ('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 
 module.exports = {
@@ -27,11 +28,45 @@ module.exports = {
                 return res.send('Please, fill all fields!')
             }
         }
+
+        if(req.files.length == 0)
+            return res.send('Please, send at least one image')
+
+        
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
-        
-        return res.redirect(`/products/${productId}` )
 
+        const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
+           
+        
+        return res.redirect(`/products/${productId}/edit` )
+
+    },
+    async show(req, res){
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+        
+        if(!product) return res.send("Product not found")
+
+        const { day, month,  hour, minutes } = date(product.update_at)
+
+        product.published = {
+            day: `${day}/${month}`,
+            hour: `${hour}h${minutes}`
+        }
+        
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+        results = await Product.files(product.id)
+        const files = results.rows.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
+        
+        return res.render('products/show', {product, files})
     },
     async edit(req, res){
         let results = await Product.find(req.params.id)
@@ -42,19 +77,48 @@ module.exports = {
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
 
+        //get categories
         results = await Category.all()
         const categories = results.rows
+
+        //get images 
+
+        results = await Product.files(product.id)
+        let files = results.rows
         
-        return res.render('products/edit.njk', { product, categories })
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`
+        }))
+
+        
+        
+        return res.render('products/edit.njk', { product, categories , files})
 
     },
     async put(req, res){
         const keys = Object.keys(req.body)
        
         for(key of keys) {
-            if (req.body[key] == '') {
+            if (req.body[key] == '' && key != "removed_files") {
                 return res.send('Please, fill all fields!')
             }
+        }
+
+        if (req.files.length != 0){
+            const newFilesPromise = req.files.map(file =>
+                File.create({...file, product_id: req.body.id})) 
+
+            await Promise.all(newFilesPromise)
+        }
+        
+        if(req.body.removed_files){
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+            await Promise.all(removedFilesPromise)
         }
         
         req.body.price = req.body.price.replace(/\D/g, "")
@@ -68,7 +132,7 @@ module.exports = {
 
         await Product.update(req.body)
 
-        return res.redirect(`/products/${req.body.id}/edit`)
+        return res.redirect(`/products/${req.body.id}`)
     },
     async delete(req, res){
         await Product.delete(req.body.id)
